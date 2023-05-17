2398425b15ae16f23d0a3d4905c60827f8e0875a
Vadim Gurkin <vadim@vgraph.ru>
2023-05-17 13:00:55 +0300

    Рабочие настройки

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,7 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set PYTORCH_CUDA_ALLOC_CONF = garbage_collection_treshol: 0.9, max_split_size_mb = 512
+set COMMANDLINE_ARGS= --theme dark --autolaunch --medvram --xformers
 
 call webui.bat
